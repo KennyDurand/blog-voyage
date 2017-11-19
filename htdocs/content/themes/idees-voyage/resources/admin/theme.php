db0758f5a08c515f286ba7b45cf107035f75c596
Kenny Durand <kenny.durand@fabernovel.com>
2017-11-19 15:49:23 +0100

    Setup Oria theme

--- htdocs/content/themes/idees-voyage/resources/admin/theme.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * Define your theme custom code.
- */
- Asset::add('screen.scss', 'css/screen.min.css', false, '1.0', 'all');
- Asset::add('theme.js', 'js/theme.min.js', false, '1.0', true, 'script'); ?>
